b9af740c9a8b1fe5cbca120aabd131e69441c32e
Gokuldas K M <gokuldas027@gmail.com>
2020-04-08 21:03:03 +0530

    listview search sett

--- lib/screen/search.dart
+++ lib/screen/search.dart
@@ -5,22 +5,61 @@ class PlantSearch extends StatefulWidget {
 
   @override
   _PlantSearchState createState() => _PlantSearchState();
-
 }
 
 class _PlantSearchState extends State<PlantSearch> {
+  List<String> entries = <String>['AAAAA', 'BBBBB', 'CCCCC', 'DDDDD'];
+  List<String> filterEntries = List();
+
+  @override
+  void initState() {
+    super.initState();
+    filterEntries = entries;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Search')
-      ),
-      body: Center(
-        child: Text('Page Under Maintenance'),
+      appBar: AppBar(title: Text('Search')),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(15.0),
+                hintText: "Hii, How're you feeling?",
+              ),
+              onChanged: (string) {
+                setState(() {
+                  filterEntries = entries
+                      .where((u) =>
+                          (u.toLowerCase().contains(string.toLowerCase())))
+                      .toList();
+                });
+              },
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: filterEntries.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Center(
+                        child: Text(
+                          '${filterEntries[index]}',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-  
 }
-  
