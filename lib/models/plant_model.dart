1a39e27b9fa4f028032578023cee822be2c2e199
Gokuldas K M <gokuldas027@gmail.com>
2020-04-16 21:03:32 +0530

    plant profile functionality sett

--- lib/models/plant_model.dart
+++ lib/models/plant_model.dart
@@ -1,22 +1,31 @@
-import 'dart:convert';
-
 class Plant {
-  final int id;
+  final String id;
   final String name;
-  final String description;
-  final bool isEdible;
+  final String sciName;
+  final String point1;
+  final String point2;
+  final String point3;
+  final String point4;
+  final String link;
 
-  Plant({this.id, this.name, this.description, this.isEdible});
+  Plant(
+      {this.id,
+      this.name,
+      this.sciName,
+      this.point1,
+      this.point2,
+      this.point3,
+      this.point4,
+      this.link});
 
   factory Plant.fromJson(Map<String, dynamic> json) => Plant(
-    id: json["id"],
-    name: json["name"],
-    description: json["description"],
-    isEdible:  json["edible"]
-  );
-}
-
-List<Plant> plantListFromJson(String str) {
-  final jsonData = json.decode(str);
-  return new List<Plant>.from(jsonData.map((x) => Plant.fromJson(x)));
+      id: json["id"],
+      name: json["name"],
+      sciName: json["sci_name"],
+      point1: json["point1"],
+      point2: json["point2"],
+      point3: json["point3"],
+      point4: json["point4"],
+      link: json["link"]
+    );
 }
--- lib/screen/plant_profile.dart
+++ lib/screen/plant_profile.dart
@@ -1,17 +1,70 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:folium_snap/models/plant_model.dart';
 
-class PlantProfile extends StatelessWidget {
+class PlantProfile extends StatefulWidget {
   final String title;
   PlantProfile({Key key, this.title}) : super(key: key);
+
+  @override
+  _PlantProfileState createState() => _PlantProfileState();
+}
+
+class _PlantProfileState extends State<PlantProfile> {
+  List<Plant> plantsList = List();
+  List<Plant> thisPlant = List();
+
+  Future<Null> getPlantsData() async {
+    final response = await rootBundle.loadString('assets/data/plants.json');
+    final responseJson = json.decode(response)['plants'];
+    setState(() {
+      for (Map user in responseJson) {
+        plantsList.add(Plant.fromJson(user));
+      }
+      thisPlant = plantsList
+          .where((u) =>
+              (u.name.toLowerCase().contains(widget.title.toLowerCase())))
+          .toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPlantsData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(title),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(12),
+              itemCount: thisPlant.length,
+              itemBuilder: (BuildContext context, int index) {
+                Plant plant = thisPlant[index];
+                return Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: <Widget>[
+                      Text(plant.name),
+                      Text(plant.point1),
+                      Text(plant.point2),
+                      Text(plant.point3),
+                      Text(plant.point4),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
